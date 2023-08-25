fb0dd1cc570065ec8703cbdd71a5eb9e74f20335
DependencyInj <somdevhari.learning@gmail.com>
2023-08-25 22:33:24 +0530

    adding some more menu items

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -2,14 +2,38 @@ import React from 'react'
 
 const SideBar = () => {
   return (
-    <div className='shadow-lg p-4 col-span-1'>
-        <h2>Explore</h2>
+    <div className='shadow-2xl p-4 col-span-1 child:pt-2'>
+      <div>
         <ul>
-            <li>Trending</li>
-            <li>Shopping</li>
-            <li>Music</li>
-            <li>Films</li>
+          <li>Home</li>
+          <li>Shorts</li>
+          <li>Subscriptions</li>
         </ul>
+      </div>
+      <div>
+        <hr className='bg-slate-500' />
+      </div>
+      <div>
+        <h2 className='font-bold'>Subscriptions</h2>
+        <ul>
+          <li>Trending</li>
+          <li>Shopping</li>
+          <li>Music</li>
+          <li>Films</li>
+        </ul>
+      </div>
+      <div>
+        <hr className='bg-slate-500' />
+      </div>
+      <div>
+        <h2 className='font-bold'>Explore</h2>
+        <ul>
+          <li>Trending</li>
+          <li>Shopping</li>
+          <li>Music</li>
+          <li>Films</li>
+        </ul>
+      </div>
     </div>
   )
 }
