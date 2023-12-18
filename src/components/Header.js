5492a8fdb7f4e53809390ba496e69dfe63cf1c81
DependencyInj <somdevhari.learning@gmail.com>
2023-12-18 08:47:42 +0530

    adding search functionality partial with debounce

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import search from '../../src/assets/images/search-interface-symbol.png'
 import { useDispatch } from 'react-redux';
 import { toggleMenu } from '../utils/navSlice';
+import { YOUTUBE_SEARCH_API } from '../utils/constants';
 
 const Header = () => {
+    const [searchQuery, setSearchQuery] = useState("");
+    console.log(searchQuery);
+
+    useEffect(()=> {
+        const timer = setTimeout(()=> getSearchSuggestions(), 200);
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [searchQuery])
+
+    const getSearchSuggestions = async () => {
+        const result = await fetch(`${YOUTUBE_SEARCH_API}${searchQuery}`);
+        const json = await result.json();
+        console.log(json[1]);
+        return json[1];
+    }
+
     const dispatch = useDispatch();
     const toggleMenuHandler = () => {
         dispatch(toggleMenu());
     }
-
-    toggleMenuHandler();
 
     return (
         <div className='grid grid-flow-col p-5 m-2 shadow-lg'>
@@ -18,7 +34,10 @@ const Header = () => {
                 <img className='h-8 mx-2' alt='youtube-logo' src={'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/1200px-YouTube_Logo_2017.svg.png'} />
             </div>
             <div className='flex w-auto'>
-                <input className='border border-gray-400 rounded-l-full w-4/6 p-1' type='text' />
+                <input className='border border-gray-400 rounded-l-full w-4/6 p-1' 
+                type='text' 
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)} />
                 <button className='border border-gray-400 rounded-r-full w-10 p-1'>
                     <img className='h-5' alt='search' src={search} />
                 </button>
