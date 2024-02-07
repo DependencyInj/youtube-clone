f473061bc477bc561c7ecd867bee33f756a3d407
DependencyInj <somdevhari.learning@gmail.com>
2024-02-07 14:19:47 +0530

    comment feature: initial commit

---
+++ src/components/CommentsContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CommentsContainer = () => {
+  return (
+    <div className='m-5'>
+        <h1 className='font-bold text-2xl'>Comments: </h1>
+    </div>
+  )
+}
+
+export default CommentsContainer;
--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { toggleMenu } from '../utils/navSlice';
 import { useSearchParams } from 'react-router-dom';
+import CommentsContainer from './CommentsContainer';
 
 const WatchPage = () => {
   const [urlSearchParams] = useSearchParams();
@@ -13,8 +14,13 @@ const WatchPage = () => {
   }, []);
 
   return (
-    <div className='px-5 '>
-      <iframe  className="rounded-xl" width="900" height="500" src={`https://www.youtube.com/embed/${urlSearchParams.get("v")}?si=9JFYb3BLXqVJLTdf`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
+    <div>
+      <div className='px-5 '>
+        <iframe className="rounded-xl" width="900" height="500" src={`https://www.youtube.com/embed/${urlSearchParams.get("v")}?si=9JFYb3BLXqVJLTdf`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
+      </div>
+      <div>
+        <CommentsContainer />
+      </div>
     </div>
   )
 }
