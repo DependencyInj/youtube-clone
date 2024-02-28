054ba3e334800abf31238f6648791bcf696b0585
DependencyInj <somdevhari.learning@gmail.com>
2024-02-28 20:16:28 +0530

    adding logic to reverse the flx of chatbox

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -10,20 +10,21 @@ const LiveChat = () => {
     const messages = useSelector(store => store.liveChat.messages);
 
     useEffect(() => {
+        let id = 1;
         const i = setInterval(() => {
             console.log("Polling");
             dispatch(addMessage({
                 name:'Hari',
-                message:'new year'
+                message:'new year' + id
             }));
+        id = id + 1;
         }, 2000);
-
         return () => clearInterval(i);
     }, []);
 
 
     return (
-        <div className='ml-2 p-2 h-[500px] bg-stone-300 w-full border border-b-slate-300 rounded-lg overflow-y-scroll'>
+        <div className='ml-2 p-2 h-[500px] bg-stone-300 w-full border border-b-slate-300 rounded-lg overflow-y-scroll flex flex-col-reverse'>
             {
                 messages.map((item, index) => <ChatMessage key={index} name={item.name} message={item.message} />)
             }
