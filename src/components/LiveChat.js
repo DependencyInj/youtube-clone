99febf018ba49f8137cdd480f27a633fde33cc75
DependencyInj <somdevhari.learning@gmail.com>
2024-03-06 08:04:26 +0530

    adding chat box feature

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,10 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import ChatMessage from "./ChatMessage";
 import { useDispatch, useSelector } from "react-redux";
 import { addMessage } from "../utils/chatSlice";
 
 const LiveChat = () => {
 
+    const [message, setMessage] = useState(null);
     const dispatch = useDispatch();
 
     const messages = useSelector(store => store.liveChat.messages);
@@ -14,21 +15,39 @@ const LiveChat = () => {
         const i = setInterval(() => {
             console.log("Polling");
             dispatch(addMessage({
-                name:'Hari',
-                message:'new year' + id
+                name: 'Hari',
+                message: 'new year' + id
             }));
-        id = id + 1;
+            id = id + 1;
         }, 2000);
         return () => clearInterval(i);
     }, []);
 
 
     return (
-        <div className='ml-2 p-2 h-[500px] bg-stone-300 w-full border border-b-slate-300 rounded-lg overflow-y-scroll flex flex-col-reverse'>
-            {
-                messages.map((item, index) => <ChatMessage key={index} name={item.name} message={item.message} />)
-            }
+        <div>
+            <div className='ml-2 p-2 h-[500px] bg-stone-300 w-full border border-b-slate-300 rounded-lg overflow-y-scroll flex flex-col-reverse'>
+                {
+                    messages.map((item, index) => <ChatMessage key={index} name={item.name} message={item.message} />)
+                }
+            </div>
+            <form className="w-full" onSubmit={(e) => {
+                e.preventDefault();
+                dispatch(addMessage({
+                    name: "Harikrishnan",
+                    message: message
+                }));
+                setMessage("");
+            }}>
+                <input className="w-[200px] border border-black" type="text"
+                    value={message}
+                    onChange={(e) => {
+                        setMessage(e.target.value);
+                    }} />
+                <button className="bg-slate-300">Send</button>
+            </form>
         </div>
+
     )
 }
 
