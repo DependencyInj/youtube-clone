999dac8446bb70df2cf81d14f763dc24ab339b67
DependencyInj <somdevhari.learning@gmail.com>
2024-03-22 06:10:45 +0530

    removing commentrd code

--- src/components/Header.js
+++ src/components/Header.js
@@ -90,17 +90,6 @@ const Header = () => {
                 <img className='h-8' alt='user' src='https://cdn-icons-png.flaticon.com/512/3177/3177440.png' />
             </div>
         </div>
-        // <div className='grid grid-flow-col p-5 m-2 shadow-lg'>
-        //     <div className='flex col-span-1'>
-        //         <img onClick={() => toggleMenuHandler()} className='h-8 cursor-pointer' alt='menu' src='https://static.vecteezy.com/system/resources/previews/002/292/406/non_2x/hamburger-menu-line-icon-free-vector.jpg' />
-        //         <img className='h-8 mx-2' alt='youtube-logo' src={'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/1200px-YouTube_Logo_2017.svg.png'} />
-        //     </div>
-
-
-        //     <div className='col-span-1'>
-        //         
-        //     </div>
-        // </div>
     )
 }
 
